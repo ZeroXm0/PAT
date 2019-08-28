1081d585ffeb85d690e2acc72f88d95c9c140256
morningM0 <zeroindexweb@gmail.com>
2019-08-28 17:07:10 +0800

    8/28 17:06

--- A_1121 Damn Single.cpp
+++ A_1121 Damn Single.cpp
@@ -12,7 +12,9 @@ int main() {
 	for(int i = 0; i < n; i++) {
 		int p1, p2;
 		scanf("%d%d", &p1, &p2);
-		Marry[p1] = p2;
+		if(p1 == 0) p1 = 111111;		//特别注意0的情况，map 对应的初始为0 
+		if(p2 == 0) p2 = 111111; 
+ 		Marry[p1] = p2;
 		Marry[p2] = p1;
 	}
 	int m;
@@ -20,21 +22,28 @@ int main() {
 	vector<int> people(m);
 	for(int i = 0; i < m; i++) {
 		scanf("%d", &people[i]);
+		if(people[i] == 0) people[i] = 111111;
 		in[people[i]] = true;
 	}
 	for(int i = 0; i < m; i++) {
 		int partner = Marry[people[i]];
-		if(!in[partner]) {
+		if(in[partner] == false) {
 			single.insert(people[i]);
 		} 
 	}
 	printf("%d\n", single.size());
 	if(single.size() > 0) {
 		set<int>::iterator it = single.begin();
-		printf("%05d", *it);
+		if( *it == 111111) {
+			printf("00000");
+		}else {
+			printf("%05d", *it);
+		}
 		it++;
 		for(; it != single.end(); it++) {
-			printf(" %d", *it);
+			if(*it == 111111) {
+				printf(" 00000");
+			}else printf(" %05d", *it);
 		}
 		printf("\n");
 	}
